de63293e40a8038041655aee1949e7554cf9cd5c
yutarofire <yutaro.ono.418@gmail.com>
2019-03-14 08:35:07 +0900

    Remove logic_gates directory

--- functions/loss_functions.py
+++ functions/loss_functions.py
@@ -13,4 +13,3 @@ def cross_entropy_error(y, t):
     # return -np.sum(t * np.log(y + 1e-7)) / batch_size
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) \
             / batch_size
-
