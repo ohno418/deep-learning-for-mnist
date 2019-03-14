fc9869c2ef9803abaeeb1fd686b99ad0484a5203
yutarofire <yutaro.ono.418@gmail.com>
2019-03-15 08:33:52 +0900

    Native layers

---
+++ layer_native.py
@@ -0,0 +1,27 @@
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        return x * y
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+        return dx, dy
+
+class AddLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        return x + y
+
+    def backward(self, dout):
+        return dout, dout
